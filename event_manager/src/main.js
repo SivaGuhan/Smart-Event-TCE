b62d017531e42a12d7157b9fa26e245337d19e52
SivaGuhan <sivasguhan@gmail.com>
2022-10-06 15:47:20 +0530

    adding main

---
+++ event_manager/src/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './main.css';
+import App from './App.js';
+
+function Main(){
+    return(
+        <div>
+            <App></App>
+        </div>
+    )
+}
+
+export default Main;
