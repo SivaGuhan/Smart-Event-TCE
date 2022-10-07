61ed730709c8fee8a2dcc1a75245f628e04d417e
SivaGuhan <sivasguhan@gmail.com>
2022-10-07 21:57:42 +0530

    tail

--- event_manager/src/main.js
+++ event_manager/src/main.js
@@ -1,13 +1,19 @@
 import React from "react";
 import './main.css';
+import ClgLo from './images/logo.png'
 
 function Main(){
     return(
         <div>
             <div className="header">
-                <h1>TCE EVENTS</h1>
+                <img id='lo' src={ClgLo} alt=''/>
+                <h1><b>TCE EVENTS</b></h1>
+            </div><br></br>
+            <div className="cursor-default">
+                <span className="text-center">
+                <h1 className="animate-pulse">Welcome</h1><br></br>
+                </span>
             </div>
-            <h1>Welcome</h1>
             <div className="welcome">
             <form className='form'>
                 <br></br>
@@ -20,7 +26,7 @@ function Main(){
                     <input type="password" id="pass" autoComplete='off' placeholder="Password"></input>
                 </div><br></br>
                  <button type="submit">Submit</button><br></br>
-                 <p>New User?? <a href='./'>Click here</a> to register</p>
+                 <p>New User?? <a href='./'><u>Click here</u></a> to register</p>
             </form>
             </div>
         </div>
