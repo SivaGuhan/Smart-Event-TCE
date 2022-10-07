b907356f6d1f284478de8320998b4ad04f490d76
SivaGuhan <sivasguhan@gmail.com>
2022-10-07 17:15:37 +0530

    sign in

--- event_manager/src/main.js
+++ event_manager/src/main.js
@@ -1,11 +1,28 @@
 import React from "react";
 import './main.css';
-import App from './App.js';
 
 function Main(){
     return(
         <div>
-            <App></App>
+            <div className="header">
+                <h1>TCE EVENTS</h1>
+            </div>
+            <h1>Welcome</h1>
+            <div className="welcome">
+            <form className='form'>
+                <br></br>
+                <p><b>Login</b></p>
+                <br></br>
+                <div className="in">
+                    <input type="email" id="email" autoComplete='off' placeholder="Username"></input>
+                </div><br></br>
+                <div className="in">
+                    <input type="password" id="pass" autoComplete='off' placeholder="Password"></input>
+                </div><br></br>
+                 <button type="submit">Submit</button><br></br>
+                 <p>New User?? <a href='./'>Click here</a> to register</p>
+            </form>
+            </div>
         </div>
     )
 }
